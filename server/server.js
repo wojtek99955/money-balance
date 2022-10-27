fab7fd2b3acefb317b08e790bdad59bacd0f806e
wojtek99955 <wojtek99955@gmail.com>
2022-10-27 16:32:40 +0200

    db connection add

--- server/server.js
+++ server/server.js
@@ -7,7 +7,12 @@ const errorHandler = require("./middleware/errorHandler");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const corsOptions = require("./config/corsOptions");
+const connectDB = require("./config/dbConn");
+const mongoose = require("mongoose");
+const { logEvents } = require("./middleware/logger");
 const PORT = process.env.PORT || 3500;
+
+connectDB();
 
 app.use(logger);
 
@@ -33,4 +38,16 @@ app.all("*", (req, res) => {
 });
 
 app.use(errorHandler);
-app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+
+mongoose.connection.once("open", () => {
+  console.log("connected to mongoDB");
+  app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+  logEvents(
+    `${err.no} : ${err.code}\t${err.syscall}\t${err.hostname}`,
+    "mongoErrLog.log"
+  );
+});

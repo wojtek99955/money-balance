e80debbc8899cc10d8b5bbd30106da1a942356df
wojtek99955 <wojtek99955@gmail.com>
2022-11-03 17:37:58 +0100

    add path logic / update styles

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,11 +1,12 @@
 import styled from "styled-components";
 import Logo from "../assets/atoms/Logo";
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 const StyledHeader = styled.header`
   position: fixed;
   width: 100%;
-  padding: 0.3rem 1rem;
+  padding: 0.8rem 1rem;
   border-bottom: 1px solid #e1e4e7;
 `;
 const Container = styled.div`
@@ -18,19 +19,36 @@ const Nav = styled.nav``;
 
 const StyledLink = styled(Link)`
   text-decoration: none;
+  &:first-of-type {
+    color: white;
+    background-color: ${({ theme }) => theme.colors.main};
+  }
+  color: ${({ theme }) => theme.colors.main};
+  border: ${({ theme }) => `2px solid ${theme.colors.main}`};
+  padding: 0.4rem 1.3rem;
+  margin-left: 1rem;
+  border-radius: 8px;
 `;
 
 const Header = () => {
+  let location = useLocation();
   return (
-    <StyledHeader>
-      <Container>
-        <Logo />
-        <Nav>
-          <StyledLink to="/sign-in">Sign In</StyledLink>
-          <StyledLink to="/sign-up">Sign Up</StyledLink>
-        </Nav>
-      </Container>
-    </StyledHeader>
+    <>
+      {location.pathname === "/sign-in" ||
+      location.pathname === "/sign-up" ? null : (
+        <>
+          <StyledHeader>
+            <Container>
+              <Logo />
+              <Nav>
+                <StyledLink to="/sign-in">Sign In</StyledLink>
+                <StyledLink to="/sign-up">Sign Up</StyledLink>
+              </Nav>
+            </Container>
+          </StyledHeader>
+        </>
+      )}
+    </>
   );
 };
 

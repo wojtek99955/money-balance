11723be71799393a5daf6cabc11393615d144a36
wojtek99955 <wojtek99955@gmail.com>
2022-11-03 14:19:32 +0100

    add app style

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,21 @@
-import React from "react";
 import { Routes, Route } from "react-router-dom";
-import SignUp from "./components/SignUp";
-import SignIn from "./components/SignIn";
+import SignUp from "./components/auth/SignUp";
+import SignIn from "./components/auth/SignIn";
+import styled from "styled-components";
+
+const AppContainer = styled.div`
+  max-width: 1600px;
+  margin: auto;
+  padding: 0 1rem;
+`;
 function App() {
   return (
-    <div className="App">
+    <AppContainer>
       <Routes>
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/sign-in" element={<SignIn />} />
       </Routes>
-    </div>
+    </AppContainer>
   );
 }
 

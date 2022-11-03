5f4944ce42df8b106f05d2630e1ab653bc578cef
wojtek99955 <wojtek99955@gmail.com>
2022-11-03 17:21:55 +0100

    add gohome

--- client/src/assets/atoms/Logo.tsx
+++ client/src/assets/atoms/Logo.tsx
@@ -1,11 +1,18 @@
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
+
 const LogoContainer = styled.div`
   font-size: 1.8rem;
   font-weight: 700;
   color: ${({ theme }) => theme.colors.main};
+  cursor: pointer;
 `;
 const Logo = () => {
-  return <LogoContainer>Money Balance</LogoContainer>;
+  let navigate = useNavigate();
+  const handleGoHome = () => {
+    navigate("/");
+  };
+  return <LogoContainer onClick={handleGoHome}>Money Balance</LogoContainer>;
 };
 
 export default Logo;

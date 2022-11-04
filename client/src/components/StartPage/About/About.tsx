90c0f1028fa634fd899eb1b65039c556b1baf4d4
wojtek99955 <wojtek99955@gmail.com>
2022-11-04 07:42:25 +0100

    refactor styles

--- client/src/components/StartPage/About/About.tsx
+++ client/src/components/StartPage/About/About.tsx
@@ -1,7 +1,11 @@
-import styled from "styled-components";
-import { BiWallet } from "react-icons/bi";
-import { BiBarChartSquare } from "react-icons/bi";
-import { BiMedal } from "react-icons/bi";
+import {
+  Container,
+  GridContainer,
+  GridItem,
+  WalletIcon,
+  ChartIcon,
+  MedalIcon,
+} from "./AboutStyle";
 
 interface Description {
   icon: JSX.Element;
@@ -9,55 +13,6 @@ interface Description {
   text: string;
 }
 
-const Container = styled.div`
-  margin: 2rem 1rem;
-  h2 {
-    color: ${({ theme }) => theme.colors.title};
-    font-size: 2rem;
-    text-align: center;
-  }
-  p {
-    color: ${({ theme }) => theme.colors.grey};
-    font-size: 1.2rem;
-    width: 20rem;
-    margin: auto;
-    padding-top: 2rem;
-  }
-`;
-
-const GridContainer = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  padding: 4rem 0;
-`;
-const WalletIcon = styled(BiWallet)`
-  font-size: 4rem;
-  color: ${({ theme }) => theme.colors.main.default};
-`;
-const ChartIcon = styled(BiBarChartSquare)`
-  font-size: 4rem;
-  color: ${({ theme }) => theme.colors.main.default};
-`;
-const MedalIcon = styled(BiMedal)`
-  font-size: 4rem;
-  color: ${({ theme }) => theme.colors.main.default};
-`;
-
-const GridItem = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  h3 {
-    font-size: 1.8rem;
-    padding: 0.5rem 0;
-  }
-  p {
-    margin-top: 2rem;
-    color: ${({ theme }) => theme.colors.title};
-    text-align: center;
-    font-size: 1rem;
-  }
-`;
 const descriptions = [
   {
     name: "Track expenses",

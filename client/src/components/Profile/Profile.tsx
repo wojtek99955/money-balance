9b68bddb0367475c56f67a64d58fb5d23f3f9faa
wojtek99955 <wojtek99955@gmail.com>
2022-11-08 11:19:56 +0100

    refactor styles

--- client/src/components/Profile/Profile.tsx
+++ client/src/components/Profile/Profile.tsx
@@ -1,64 +1,13 @@
-import styled from "styled-components";
-import { FiChevronDown } from "react-icons/fi";
-import { FiChevronUp } from "react-icons/fi";
 import { useState } from "react";
-import { motion } from "framer-motion";
 import { AnimatePresence } from "framer-motion";
 import { useNavigate } from "react-router-dom";
-
-const Container = styled.div`
-  position: absolute;
-  bottom: 1.5rem;
-`;
-
-const DownIcon = styled(FiChevronDown)`
-  color: white;
-  font-size: 1.5rem;
-`;
-
-const UpIcon = styled(FiChevronUp)`
-  color: white;
-  font-size: 1.5rem;
-`;
-
-const OptionsContainer = styled(motion.div)`
-  background-color: ${({ theme }) => theme.colors.main.light};
-  border-radius: 12px;
-  padding: 1rem 0;
-  overflow: hidden;
-  height: 200px;
-
-  ul {
-    list-style: none;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    height: 100%;
-    li {
-      color: white;
-      padding: 0.6rem 1rem;
-      cursor: pointer;
-      &:hover {
-        background-color: ${({ theme }) => theme.colors.main.hover};
-      }
-    }
-  }
-`;
-const ProfileWrapper = styled.div`
-  padding: 1.1rem 0.2rem;
-  border-radius: 12px;
-  background-color: ${({ theme }) => theme.colors.main.default};
-  width: 11rem;
-  overflow-wrap: break-word;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  cursor: pointer;
-  span {
-    color: white;
-    user-select: none;
-  }
-`;
+import {
+  Container,
+  DownIcon,
+  UpIcon,
+  OptionsContainer,
+  ProfileWrapper,
+} from "./ProfileStyle";
 
 const username = JSON.parse(localStorage.getItem("username")!);
 console.log(username);

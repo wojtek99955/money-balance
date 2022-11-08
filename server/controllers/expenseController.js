de8c70b9af8819d04670a08456a63357a52b853a
wojtek99955 <wojtek99955@gmail.com>
2022-11-08 16:56:42 +0100

    add expense controllers

---
+++ server/controllers/expenseController.js
@@ -0,0 +1,33 @@
+const Expense = require("../models/Expense");
+const jwt_decode = require("jwt-decode");
+const asyncHandler = require("express-async-handler");
+
+const createExpense = async (req, res) => {
+  const { username, category, amount } = req.body;
+
+  const expense = await Expense.create({ category, username, amount });
+
+  if (expense) {
+    return res.status(201).json({ message: "New expense created" });
+  } else {
+    return res.status(400).json({ message: "Invalid expense data received" });
+  }
+};
+
+const getExpenses = asyncHandler(async (req, res) => {
+  let JWT = req.cookies.jwt;
+
+  const decoded = jwt_decode(JWT);
+  const username = decoded.username;
+
+  const expenses = await Expense.find({ username: username })
+    .select("-username")
+    .lean();
+
+  res.json({ expenses });
+});
+
+module.exports = {
+  createExpense,
+  getExpenses,
+};

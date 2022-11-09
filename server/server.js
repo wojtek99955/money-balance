f4914ad5d4970214fe1618bd4cf7fc2fb640a020
wojtek99955 <wojtek99955@gmail.com>
2022-11-09 11:03:25 +0100

    routes added

--- server/server.js
+++ server/server.js
@@ -25,7 +25,10 @@ app.use(cookieParser());
 app.use("/", express.static(path.join(__dirname, "public")));
 
 app.use("/", require("./routes/root"));
+app.use("/auth", require("./routes/authRoutes"));
 app.use("/users", require("./routes/userRoutes"));
+app.use("/income", require("./routes/incomeRoutes"));
+app.use("/expense", require("./routes/expenseRoutes"));
 
 app.all("*", (req, res) => {
   res.status(404);

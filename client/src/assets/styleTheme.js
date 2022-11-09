bb3c9ecbcbf2c47144d1fa3d8bd5604cb7315d2a
wojtek99955 <wojtek99955@gmail.com>
2022-11-09 10:56:44 +0100

    update theme

--- client/src/assets/styleTheme.js
+++ client/src/assets/styleTheme.js
@@ -1,11 +1,16 @@
 export const theme = {
   colors: {
-    main: { default: "#397EF6", hover: "#0a5ae5", active: "#70a2f9" },
+    main: {
+      default: "#397EF6",
+      hover: "#0a5ae5",
+      active: "#70a2f9",
+      light: "#70a2f9",
+    },
     title: "#374257",
     grey: "#bfc0c5",
     lightGrey: "#eff0f6",
     backgroundColor: {
-      lightBackground: "#fafbfc",
+      lightBackground: "#E7EAF2",
     },
   },
 };

2a67a36d5511cd5095d7038a275870fa717e36c1
wojtek99955 <wojtek99955@gmail.com>
2022-11-09 16:19:17 +0100

    update user routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,12 +1,14 @@
-const express = require("express");
-const router = express.Router();
-const usersController = require("../controllers/usersController");
+const express = require('express')
+const router = express.Router()
+const usersController = require('../controllers/usersController')
+const verifyJWT = require('../middleware/verifyJWT')
 
-router
-  .route("/")
-  .get(usersController.getAllUsers)
-  .post(usersController.createNewUser)
-  .patch(usersController.updateUser)
-  .delete(usersController.deleteUser);
+// router.use(verifyJWT)
 
-module.exports = router;
+router.route('/')
+    .get(usersController.getAllUsers)
+    .post(usersController.createNewUser)
+    .patch(usersController.updateUser)
+    .delete(usersController.deleteUser)
+
+module.exports = router

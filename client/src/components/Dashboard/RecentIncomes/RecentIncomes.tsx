eecb63ccdbb6133b3f32f56e68e5c16e8c39981f
wojtek99955 <wojtek99955@gmail.com>
2022-11-10 12:09:17 +0100

    update content/styles

--- client/src/components/Dashboard/RecentIncomes/RecentIncomes.tsx
+++ client/src/components/Dashboard/RecentIncomes/RecentIncomes.tsx
@@ -1,6 +1,45 @@
 import { DashboardBox } from "../../../assets/atoms/DashboardBox";
 import { useGetIncomesQuery } from "../../../api/apiSlice";
 import { getIncomeCategoryIcon } from "../../../helpers/getIncomeCategoryIcon";
+import styled from "styled-components";
+
+export const IncomesWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 2rem;
+  width: 100%;
+  gap: 1rem;
+`;
+export const Income = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  align-items: center;
+  gap: 2rem;
+`;
+
+const IncomeDataGroup = styled.div`
+  display: flex;
+  gap: 1rem;
+  align-items: center;
+  div {
+    color: ${({ theme }) => theme.colors.title};
+    font-weight: 700;
+    font-size: 1.1rem;
+    text-transform: capitalize;
+  }
+  span {
+    color: ${({ theme }) => theme.colors.grey};
+    font-weight: 500;
+    font-size: 1rem;
+  }
+`;
+
+const Amount = styled.div`
+  color: green;
+  font-size: 1.2rem;
+  font-weight: 600;
+`;
 
 const RecentIncomes = () => {
   const { data: incomes, isLoading } = useGetIncomesQuery(undefined);
@@ -8,14 +47,22 @@ const RecentIncomes = () => {
   return (
     <DashboardBox>
       <h3>Recent incomes</h3>
-      {incomes?.incomes!.map((income: any) => {
-        return (
-          <div>
-            <div>{getIncomeCategoryIcon(income.category)}</div>
-            <div>{income.category}</div>
-          </div>
-        );
-      })}
+      <IncomesWrapper>
+        {incomes?.incomes!.map((income: any) => {
+          return (
+            <Income>
+              <IncomeDataGroup>
+                {getIncomeCategoryIcon(income.category)}
+                <div>
+                  <div>{income.category}</div>
+                  <span>{income.date}</span>
+                </div>
+              </IncomeDataGroup>
+              <Amount> + {income.amount} $</Amount>
+            </Income>
+          );
+        })}
+      </IncomesWrapper>
     </DashboardBox>
   );
 };

7038d78e1fa287aa652a652b1428c5a60bd93d77
wojtek99955 <wojtek99955@gmail.com>
2022-11-15 00:01:25 +0100

    create expense slice

---
+++ client/src/api/expenseApiSlice.ts
@@ -0,0 +1,57 @@
+import { apiSlice } from "./apiSlice";
+
+const expenseApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getExpenses: builder.query<any, any>({
+      query: (page = 0) => `/expense?p=${page}`,
+      providesTags: ["Expenses"],
+    }),
+    addExpenses: builder.mutation({
+      query: (expense) => ({
+        url: "/expense",
+        method: "POST",
+        body: expense,
+      }),
+      invalidatesTags: ["Expenses"],
+    }),
+    deleteExpense: builder.mutation({
+      query: ({ id }) => ({
+        url: "/expense",
+        method: "DELETE",
+        body: { id },
+      }),
+      invalidatesTags: ["Expenses"],
+    }),
+    updateExpense: builder.mutation({
+      query: ({ id, amount, username, category }) => ({
+        url: "/expense",
+        method: "PATCH",
+        body: {
+          id,
+          amount,
+          username,
+          category,
+        },
+      }),
+      invalidatesTags: ["Expenses"],
+    }),
+    getLatestExpenses: builder.query<any, undefined>({
+      query: () => "/expense/latest",
+      providesTags: ["Expenses"],
+    }),
+    getTotalExpense: builder.query<any, undefined>({
+      query: () => "expense/total",
+      providesTags: ["Expenses"],
+    }),
+  }),
+  overrideExisting: false,
+});
+
+export const {
+  useGetExpensesQuery,
+  useAddExpensesMutation,
+  useUpdateExpenseMutation,
+  useGetLatestExpensesQuery,
+  useGetTotalExpenseQuery,
+  useDeleteExpenseMutation,
+} = expenseApiSlice;

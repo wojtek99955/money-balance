c7fa72a02fade3d5874945b7eec5591e17ae4c76
wojtek99955 <wojtek99955@gmail.com>
2022-11-15 10:34:00 +0100

    refactor summary styles

--- client/src/components/Dashboard/Summary/Summary.tsx
+++ client/src/components/Dashboard/Summary/Summary.tsx
@@ -1,74 +1,20 @@
-import styled from "styled-components";
-import { TbWallet } from "react-icons/tb";
-import { GiReceiveMoney } from "react-icons/gi";
-import { GiExpense } from "react-icons/gi";
-import { IoMdAddCircleOutline } from "react-icons/io";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import ExpensesModal from "../ExpensesModal/ExpensesModal";
 import IncomesModal from "../IncomesModal/IncomesModal";
 import { DashboardBox } from "../../../assets/atoms/DashboardBox";
 import { useGetTotalIncomeQuery } from "../../../api/incomeApiSlice";
 import { useGetTotalExpenseQuery } from "../../../api/expenseApiSlice";
-
-const BoxWrapper = styled.div`
-  display: flex;
-  justify-content: space-between;
-`;
-
-const IconContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 3.5rem;
-  height: 3.5rem;
-  background-color: ${({ color }) => color};
-  border-radius: 12px;
-  cursor: pointer;
-`;
-const WalletIcon = styled(TbWallet)`
-  font-size: 2rem;
-  color: #ff9999;
-`;
-
-const IncomeIcon = styled(GiReceiveMoney)`
-  font-size: 2rem;
-  color: #25c196;
-`;
-
-const ItemContainer = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 1rem;
-  h3 {
-    color: ${({ theme }) => theme.colors.grey};
-  }
-  &:not(:first-of-type) {
-    cursor: pointer;
-  }
-`;
-
-const ExpenseIcon = styled(GiExpense)`
-  font-size: 2rem;
-  color: ${({ theme }) => theme.colors.main.default};
-`;
-
-const AddIcon = styled(IoMdAddCircleOutline)`
-  font-size: 1.6rem;
-  color: ${({ theme }) => theme.colors.grey};
-  cursor: pointer;
-`;
-
-const Value = styled.div`
-  font-size: 1.2rem;
-  color: ${({ theme }) => theme.colors.title};
-  font-weight: 600;
-`;
-
-const ValueContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
+import {
+  BoxWrapper,
+  IconContainer,
+  WalletIcon,
+  IncomeIcon,
+  ItemContainer,
+  ExpenseIcon,
+  AddIcon,
+  Value,
+  ValueContainer,
+} from "./SummaryStyle";
 
 const Summary = () => {
   const [openExpensesModal, setOpenExpensesModal] = useState(false);
---
+++ client/src/components/Dashboard/Summary/SummaryStyle.ts
@@ -0,0 +1,65 @@
+import styled from "styled-components";
+import { TbWallet } from "react-icons/tb";
+import { GiReceiveMoney } from "react-icons/gi";
+import { GiExpense } from "react-icons/gi";
+import { IoMdAddCircleOutline } from "react-icons/io";
+
+export const BoxWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const IconContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 3.5rem;
+  height: 3.5rem;
+  background-color: ${({ color }) => color};
+  border-radius: 12px;
+  cursor: pointer;
+`;
+export const WalletIcon = styled(TbWallet)`
+  font-size: 2rem;
+  color: #ff9999;
+`;
+
+export const IncomeIcon = styled(GiReceiveMoney)`
+  font-size: 2rem;
+  color: #25c196;
+`;
+
+export const ItemContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  h3 {
+    color: ${({ theme }) => theme.colors.grey};
+  }
+  &:not(:first-of-type) {
+    cursor: pointer;
+  }
+`;
+
+export const ExpenseIcon = styled(GiExpense)`
+  font-size: 2rem;
+  color: ${({ theme }) => theme.colors.main.default};
+`;
+
+export const AddIcon = styled(IoMdAddCircleOutline)`
+  font-size: 1.6rem;
+  color: ${({ theme }) => theme.colors.grey};
+  cursor: pointer;
+`;
+
+export const Value = styled.div`
+  font-size: 1.2rem;
+  color: ${({ theme }) => theme.colors.title};
+  font-weight: 600;
+`;
+
+export const ValueContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;

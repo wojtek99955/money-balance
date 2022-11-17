856828b2c7fb198d512746e424177b4201474594
wojtek99955 <wojtek99955@gmail.com>
2022-11-17 12:21:33 +0100

    style update

--- client/src/assets/atoms/LoaderContainer.tsx
+++ client/src/assets/atoms/LoaderContainer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
-  background-color: #f6f6f6;
+  background-color: #eee;
   width: 100%;
   height: 100%;
   position: absolute;

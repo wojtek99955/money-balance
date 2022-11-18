740b8d2450e05af323462b0485514c9b16875bd6
wojtek99955 <wojtek99955@gmail.com>
2022-11-18 14:57:05 +0100

    add date input

--- client/src/components/IncomesList/IncomesFilterDropdown/IncomesFilterDropdown.tsx
+++ client/src/components/IncomesList/IncomesFilterDropdown/IncomesFilterDropdown.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import styled from "styled-components";
+import { DateInput } from "../../../assets/atoms/DateInput";
+import DateInputContainer from "../../../assets/atoms/DateInputContainer";
 import FilterDropdown from "../../../assets/atoms/FilterDropdown";
 import { FilterWallet } from "../../../Interfaces/FilterWallet";
 
@@ -9,6 +11,12 @@ interface Props {
 }
 
 const FilterContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 3rem;
+`;
+
+const DropdownWrapper = styled.div`
   display: flex;
   gap: 1rem;
 `;
@@ -26,83 +34,93 @@ const IncomesFilterDropdown = ({ setFilterData, filterData }: Props) => {
 
   return (
     <FilterContainer>
-      <FilterDropdown filterName="Category" selectedValue={selectedCategory}>
-        <ul>
-          <li
-            onClick={() => {
-              setFilterData({ ...filterData, category: "all" });
-              setSelectedCategory("All");
-            }}
-          >
-            All
-          </li>
-          <li
-            onClick={() => {
-              setFilterData({ ...filterData, category: "salary" });
-              setSelectedCategory("Salary");
-            }}
-          >
-            Salary
-          </li>
-          <li
-            onClick={() => {
-              setFilterData({ ...filterData, category: "prize" });
-              setSelectedCategory("Prize");
-            }}
-          >
-            Prize
-          </li>
-        </ul>
-      </FilterDropdown>
-      <FilterDropdown filterName="Date" selectedValue={selectedAddedTime}>
-        <ul>
-          <li
-            onClick={() => {
-              setFilterData({ ...filterData, timestamp: -1 });
-              setSelectedAddedTime("Latest");
-            }}
-          >
-            Latest
-          </li>
-          <li
-            onClick={() => {
-              setFilterData({ ...filterData, timestamp: 1 });
-              setSelectedAddedTime("Oldest");
-            }}
-          >
-            Oldest
-          </li>
-        </ul>
-      </FilterDropdown>
-      <FilterDropdown filterName="Items" selectedValue={selectedItemsPerPage}>
-        <ul>
-          <li
-            onClick={() => {
-              setFilterData({ ...filterData, limit: 5 });
-              setSelectedItemsPerPage(5);
-            }}
-          >
-            5
-          </li>
-          <li
-            onClick={() => {
-              setFilterData({ ...filterData, limit: 10 });
-              setSelectedItemsPerPage(10);
-            }}
-          >
-            10
-          </li>
-          <li
-            onClick={() => {
-              setFilterData({ ...filterData, limit: 15 });
-              setSelectedItemsPerPage(15);
-            }}
-          >
-            15
-          </li>
-        </ul>
-      </FilterDropdown>
-      <input type="date" name="" id="" onChange={pickDate} />
+      <DateInputContainer date={filterData.date}>
+        <DateInput
+          type="date"
+          name=""
+          id=""
+          onChange={pickDate}
+          placeholder="date"
+        />
+      </DateInputContainer>
+      <DropdownWrapper>
+        <FilterDropdown filterName="Category" selectedValue={selectedCategory}>
+          <ul>
+            <li
+              onClick={() => {
+                setFilterData({ ...filterData, category: "all" });
+                setSelectedCategory("All");
+              }}
+            >
+              All
+            </li>
+            <li
+              onClick={() => {
+                setFilterData({ ...filterData, category: "salary" });
+                setSelectedCategory("Salary");
+              }}
+            >
+              Salary
+            </li>
+            <li
+              onClick={() => {
+                setFilterData({ ...filterData, category: "prize" });
+                setSelectedCategory("Prize");
+              }}
+            >
+              Prize
+            </li>
+          </ul>
+        </FilterDropdown>
+        <FilterDropdown filterName="Date" selectedValue={selectedAddedTime}>
+          <ul>
+            <li
+              onClick={() => {
+                setFilterData({ ...filterData, timestamp: -1 });
+                setSelectedAddedTime("Latest");
+              }}
+            >
+              Latest
+            </li>
+            <li
+              onClick={() => {
+                setFilterData({ ...filterData, timestamp: 1 });
+                setSelectedAddedTime("Oldest");
+              }}
+            >
+              Oldest
+            </li>
+          </ul>
+        </FilterDropdown>
+        <FilterDropdown filterName="Items" selectedValue={selectedItemsPerPage}>
+          <ul>
+            <li
+              onClick={() => {
+                setFilterData({ ...filterData, limit: 5 });
+                setSelectedItemsPerPage(5);
+              }}
+            >
+              5
+            </li>
+            <li
+              onClick={() => {
+                setFilterData({ ...filterData, limit: 10 });
+                setSelectedItemsPerPage(10);
+              }}
+            >
+              10
+            </li>
+            <li
+              onClick={() => {
+                setFilterData({ ...filterData, limit: 15 });
+                setSelectedItemsPerPage(15);
+              }}
+            >
+              15
+            </li>
+          </ul>
+        </FilterDropdown>
+      </DropdownWrapper>
     </FilterContainer>
   );
 };

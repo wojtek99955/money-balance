d5ca06cc5047ff0b6774e7a3df49731d565d9945
wojtek99955 <wojtek99955@gmail.com>
2022-11-19 11:49:54 +0100

    refactor styles

--- client/src/components/Dashboard/Sidebar/Sidebar.tsx
+++ client/src/components/Dashboard/Sidebar/Sidebar.tsx
@@ -1,86 +1,18 @@
-import styled from "styled-components";
 import Logo from "../../../assets/atoms/Logo";
-import { TbLayoutDashboard } from "react-icons/tb";
-import { Link } from "react-router-dom";
-import { TbChartBar } from "react-icons/tb";
-import { TbCalendar } from "react-icons/tb";
-import { TbWallet } from "react-icons/tb";
-import { FaListUl } from "react-icons/fa";
 import Profile from "../../Profile/Profile";
-import { IoMdAddCircleOutline } from "react-icons/io";
-
-const Container = styled.aside`
-  height: 100vh;
-  width: 13rem;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  position: relative;
-  background-color: white;
-`;
-
-const Nav = styled.nav`
-  margin-top: 2rem;
-`;
-
-const DashboardIcon = styled(TbLayoutDashboard)`
-  font-size: 2rem;
-  color: ${({ theme }) => theme.colors.main.default};
-`;
-
-const StyledLink = styled(Link)`
-  text-decoration: none;
-  display: flex;
-  align-items: center;
-  gap: 0.3rem;
-  color: ${({ theme }) => theme.colors.title};
-  font-size: 1.1rem;
-  margin-bottom: 1rem;
-  user-select: none;
-`;
-
-const ChartIcon = styled(TbChartBar)`
-  font-size: 2rem;
-  color: ${({ theme }) => theme.colors.main.default};
-`;
-
-const CalendarIcon = styled(TbCalendar)`
-  font-size: 2rem;
-  color: ${({ theme }) => theme.colors.main.default};
-`;
-
-const WalletIcon = styled(TbWallet)`
-  font-size: 2rem;
-  color: ${({ theme }) => theme.colors.main.default};
-`;
-
-const CategoryIcon = styled(FaListUl)`
-  font-size: 2rem;
-  color: ${({ theme }) => theme.colors.main.default};
-`;
-
-const AddIcon = styled(IoMdAddCircleOutline)`
-  font-size: 2rem;
-  color: ${({ theme }) => theme.colors.main.default};
-`;
-
-const CategoryDropdown = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 0.3rem;
-  color: ${({ theme }) => theme.colors.title};
-  font-size: 1.1rem;
-  margin-bottom: 1rem;
-  user-select: none;
-`;
-
-const SidebarWrapper = styled.div`
-  position: fixed;
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
+import {
+  Container,
+  Nav,
+  DashboardIcon,
+  StyledLink,
+  ChartIcon,
+  CalendarIcon,
+  WalletIcon,
+  CategoryIcon,
+  AddIcon,
+  CategoryDropdown,
+  SidebarWrapper,
+} from "./SidebarStyle";
 
 const links = [
   {
---
+++ client/src/components/Dashboard/Sidebar/SidebarStyle.ts
@@ -0,0 +1,82 @@
+import styled from "styled-components";
+import { TbLayoutDashboard } from "react-icons/tb";
+import { Link } from "react-router-dom";
+import { TbChartBar } from "react-icons/tb";
+import { TbCalendar } from "react-icons/tb";
+import { TbWallet } from "react-icons/tb";
+import { FaListUl } from "react-icons/fa";
+import { IoMdAddCircleOutline } from "react-icons/io";
+
+export const Container = styled.aside`
+  height: 100vh;
+  width: 13rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+  background-color: white;
+`;
+
+export const Nav = styled.nav`
+  margin-top: 2rem;
+`;
+
+export const DashboardIcon = styled(TbLayoutDashboard)`
+  font-size: 1.5rem;
+  color: ${({ theme }) => theme.colors.main.default};
+`;
+
+export const StyledLink = styled(Link)`
+  text-decoration: none;
+  display: flex;
+  align-items: center;
+  gap: 0.3rem;
+  color: ${({ theme }) => theme.colors.title};
+  font-size: 1.1rem;
+  margin-bottom: 1rem;
+  user-select: none;
+`;
+
+export const ChartIcon = styled(TbChartBar)`
+  font-size: 1.5rem;
+  color: ${({ theme }) => theme.colors.main.default};
+`;
+
+export const CalendarIcon = styled(TbCalendar)`
+  font-size: 1.5rem;
+  color: ${({ theme }) => theme.colors.main.default};
+`;
+
+export const WalletIcon = styled(TbWallet)`
+  font-size: 1.5rem;
+  color: ${({ theme }) => theme.colors.main.default};
+`;
+
+export const CategoryIcon = styled(FaListUl)`
+  font-size: 1.5rem;
+  color: ${({ theme }) => theme.colors.main.default};
+`;
+
+export const AddIcon = styled(IoMdAddCircleOutline)`
+  font-size: 1.5rem;
+  color: ${({ theme }) => theme.colors.main.default};
+`;
+
+export const CategoryDropdown = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.3rem;
+  color: ${({ theme }) => theme.colors.title};
+  font-size: 1.1rem;
+  margin-bottom: 1rem;
+  user-select: none;
+`;
+
+export const SidebarWrapper = styled.div`
+  position: fixed;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 1rem 0;
+`;

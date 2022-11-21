370e154ce3a57694fa8e72775aaa895baf2182e5
wojtek99955 <wojtek99955@gmail.com>
2022-11-21 19:18:39 +0100

    update styles

--- client/src/components/Dashboard/RecentTransactionsCharts/RecentTransactionsCharts.tsx
+++ client/src/components/Dashboard/RecentTransactionsCharts/RecentTransactionsCharts.tsx
@@ -4,10 +4,23 @@ import styled from "styled-components";
 import RecentExpensesChart from "./RecentExpensesChart";
 import RecentIncomesChart from "./RecentIncomesChart";
 
-const Button = styled.button``;
+const Button = styled.button`
+  border: none;
+  border-radius: 8px;
+  padding: 0.6rem 0.8rem;
+  cursor: pointer;
+`;
 const BtnsContainer = styled.div`
   display: flex;
+  justify-content: flex-end;
+  gap: 1rem;
+  margin-bottom: 1rem;
 `;
+
+const Wrapper = styled.div`
+  padding: 1rem 0;
+`;
+
 const RecentTransactionsCharts = () => {
   const [currentChart, setCurrentChart] = useState<"expenses" | "incomes">(
     "expenses"
@@ -21,15 +34,17 @@ const RecentTransactionsCharts = () => {
   };
   return (
     <DashboardBox>
-      <BtnsContainer>
-        <Button onClick={showExpensesChart}>Expenses</Button>
-        <Button onClick={showIncomesChart}>Incomes</Button>
-      </BtnsContainer>
-      {currentChart === "expenses" ? (
-        <RecentExpensesChart />
-      ) : (
-        <RecentIncomesChart />
-      )}
+      <Wrapper>
+        <BtnsContainer>
+          <Button onClick={showExpensesChart}>Expenses</Button>
+          <Button onClick={showIncomesChart}>Incomes</Button>
+        </BtnsContainer>
+        {currentChart === "expenses" ? (
+          <RecentExpensesChart />
+        ) : (
+          <RecentIncomesChart />
+        )}
+      </Wrapper>
     </DashboardBox>
   );
 };

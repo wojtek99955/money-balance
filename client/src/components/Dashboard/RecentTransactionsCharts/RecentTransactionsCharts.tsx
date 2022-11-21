a5bfafe8b4ecdd35e936dcd0a15dfd9e87e55550
wojtek99955 <wojtek99955@gmail.com>
2022-11-21 19:01:23 +0100

    add charts logic

--- client/src/components/Dashboard/RecentTransactionsCharts/RecentTransactionsCharts.tsx
+++ client/src/components/Dashboard/RecentTransactionsCharts/RecentTransactionsCharts.tsx
@@ -2,19 +2,34 @@ import { DashboardBox } from "../../../assets/atoms/DashboardBox";
 import { useState } from "react";
 import styled from "styled-components";
 import RecentExpensesChart from "./RecentExpensesChart";
+import RecentIncomesChart from "./RecentIncomesChart";
 
 const Button = styled.button``;
 const BtnsContainer = styled.div`
   display: flex;
 `;
 const RecentTransactionsCharts = () => {
+  const [currentChart, setCurrentChart] = useState<"expenses" | "incomes">(
+    "expenses"
+  );
+
+  const showExpensesChart = () => {
+    setCurrentChart("expenses");
+  };
+  const showIncomesChart = () => {
+    setCurrentChart("incomes");
+  };
   return (
     <DashboardBox>
       <BtnsContainer>
-        <Button>Expenses</Button>
-        <Button>Incomes</Button>
+        <Button onClick={showExpensesChart}>Expenses</Button>
+        <Button onClick={showIncomesChart}>Incomes</Button>
       </BtnsContainer>
-      <RecentExpensesChart />
+      {currentChart === "expenses" ? (
+        <RecentExpensesChart />
+      ) : (
+        <RecentIncomesChart />
+      )}
     </DashboardBox>
   );
 };

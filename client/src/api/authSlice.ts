6f5d97a77921d217fb8c955eb636d5ad1de01199
wojtek99955 <wojtek99955@gmail.com>
2022-11-22 18:57:58 +0100

    add auth slice

---
+++ client/src/api/authSlice.ts
@@ -0,0 +1,21 @@
+import { apiSlice } from "./apiSlice";
+
+export const authSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (credentials) => ({
+        url: "/auth",
+        method: "POST",
+        body: { ...credentials },
+      }),
+    }),
+    logout: builder.mutation({
+      query: () => ({
+        url: "/auth/logout",
+        method: "POST",
+      }),
+    }),
+  }),
+});
+
+export const { useLoginMutation, useLogoutMutation } = authSlice;

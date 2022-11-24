e6922906dc1bffe4d8b9f391f0688948249297b6
wojtek99955 <wojtek99955@gmail.com>
2022-11-24 10:53:18 +0100

    add avatar middleware

---
+++ server/middleware/uploadAvatar.js
@@ -0,0 +1,16 @@
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads");
+  },
+  filename: (req, file, cb) => {
+    console.log(file);
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+const uploadAvatar = multer({ storage: storage });
+
+module.exports = uploadAvatar;

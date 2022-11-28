e51a20567143df59d5cc0f64d9d65921a1f22705
wojtek99955 <wojtek99955@gmail.com>
2022-11-28 13:04:04 +0100

    refactor style

--- client/src/components/Profile/Profile.tsx
+++ client/src/components/Profile/Profile.tsx
@@ -1,4 +1,3 @@
-import styled from "styled-components";
 import { RouteContainer } from "../../assets/atoms/RouteContainer";
 import { useState } from "react";
 import { useEffect } from "react";
@@ -7,90 +6,18 @@ import {
   useAddAvatarMutation,
   useDeleteAvatarMutation,
 } from "../../api/avatarSlice";
-import { BsCamera } from "react-icons/bs";
-import { motion } from "framer-motion";
-import { CgProfile } from "react-icons/cg";
-import { MdOutlineDeleteForever } from "react-icons/md";
 import { useGetUserQuery } from "../../api/userSlice";
+import {
+  Avatar,
+  ProfileIcon,
+  AddAvatarIconContainer,
+  AddAvatarIcon,
+  FileInput,
+  EditContainer,
+  DeleteIcon,
+  variants,
+} from "./ProfileStyle";
 
-interface Img {
-  img: string | number;
-}
-
-interface FetchAvatar {
-  avatar: any;
-}
-
-const Avatar = styled(motion.div)<Img>`
-  width: 15rem;
-  height: 15rem;
-  border: 1px solid grey;
-  border-radius: 12px;
-  background-image: ${({ img }) => `url(${img})`};
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
-  position: relative;
-  cursor: pointer;
-`;
-
-const ProfileIcon = styled(CgProfile)`
-  font-size: 10rem;
-  color: ${({ theme }) => theme.colors.grey};
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
-`;
-
-const AddAvatarIconContainer = styled(motion.div)`
-  background-color: white;
-  position: absolute;
-  width: 4rem;
-  height: 4rem;
-  border-radius: 50%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  cursor: pointer;
-  z-index: 2;
-  right: -1.5rem;
-  bottom: -2.5rem;
-`;
-const AddAvatarIcon = styled(BsCamera)`
-  font-size: 1.5rem;
-  color: grey;
-`;
-
-const FileInput = styled.input<FetchAvatar>`
-  display: block;
-  height: 100%;
-  opacity: 0;
-  cursor: pointer;
-  position: relative;
-  z-index: ${({ avatar }) => (avatar === 0 ? "100" : "1")};
-`;
-
-const EditContainer = styled.div`
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.4);
-  top: 0;
-  border-radius: 12px;
-  z-index: 10;
-  button {
-  }
-`;
-
-const DeleteIcon = styled(MdOutlineDeleteForever)`
-  font-size: 2rem;
-  color: white;
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
-`;
 const Profile = () => {
   const [files, setFiles] = useState<any>(null);
   const [addAvatar, response] = useAddAvatarMutation();
@@ -125,17 +52,6 @@ const Profile = () => {
   function handleMouseLeave() {
     setIsHovered(false);
   }
-  const variants = {
-    hover: {
-      left: "50%",
-      transform: "translate(-50%, -50%)",
-      top: "50%",
-    },
-    initial: {
-      right: "-1.5rem",
-      bottom: "-2.5rem",
-    },
-  };
 
   const { data: userData, isLoading: userDataLoading } =
     useGetUserQuery(undefined);

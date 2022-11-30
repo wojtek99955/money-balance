bc8b07e8c0805bf8d6a77798346481c1dde3c758
wojtek99955 <wojtek99955@gmail.com>
2022-11-30 14:14:28 +0100

    update routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -9,7 +9,7 @@ router
   .route("/")
   .get(usersController.getUser)
   .post(usersController.createNewUser)
-  .patch(usersController.updateUser)
+  .patch(usersController.updateUsername)
   .delete(usersController.deleteUser);
 
 module.exports = router;

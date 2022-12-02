cea7f1be200405d64f34f2f40ba416cb9e880453
wojtek99955 <wojtek99955@gmail.com>
2022-12-02 15:19:30 +0100

    refactor style

---
+++ client/src/components/Dashboard/ExpensesModal/ExpensesModalStyle.ts
@@ -0,0 +1,65 @@
+import styled from "styled-components";
+import { motion } from "framer-motion";
+import { RiCloseCircleLine } from "react-icons/ri";
+import { Formik, Form, Field } from "formik";
+
+export const Container = styled(motion.div)`
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.3);
+  position: fixed;
+  top: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const FormContainer = styled(motion.div)`
+  background-color: white;
+  width: 400px;
+  height: auto;
+  padding: 2rem 0;
+  border-radius: 12px;
+  position: relative;
+
+  h3 {
+    text-align: center;
+    font-size: 1.6rem;
+    padding-bottom: 2rem;
+  }
+
+  select {
+    display: block;
+    border: ${({ theme }) => `2px solid ${theme.colors.grey}`};
+    border-radius: 12px;
+    padding: 1rem;
+    width: 80%;
+    font-size: 1rem;
+  }
+`;
+
+export const CloseIcon = styled(RiCloseCircleLine)`
+  font-size: 1.6rem;
+  color: ${({ theme }) => theme.colors.grey};
+  cursor: pointer;
+  position: absolute;
+  top: 0.5rem;
+  right: 0.5rem;
+`;
+
+export const StyledField = styled(Field)`
+  display: block;
+  border: ${({ theme }) => `2px solid ${theme.colors.grey}`};
+  border-radius: 12px;
+  padding: 1rem;
+  width: 80%;
+  font-size: 1rem;
+`;
+
+export const FormWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  justify-content: center;
+  align-items: center;
+`;

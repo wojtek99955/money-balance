c4c6002e95ae4f56449fce398d03bfa0e875463c
wojtek99955 <wojtek99955@gmail.com>
2022-12-09 14:26:36 +0100

    add goal controller

---
+++ server/controllers/goalController.js
@@ -0,0 +1,32 @@
+const Goal = require("../models/Goal");
+const asyncHandler = require("express-async-handler");
+const jwt_decode = require("jwt-decode");
+
+const createNewGoal = async (req, res) => {
+  const date = new Date()
+    .toLocaleDateString("pt-br")
+    .split("/")
+    .reverse()
+    .join("-");
+  const { description, amount, deposit } = req.body;
+
+  let JWT = req.cookies.jwt;
+  const decoded = jwt_decode(JWT);
+  const userId = decoded.userId;
+
+  const goal = await Goal.create({
+    description,
+    userId,
+    amount,
+    date,
+    deposit,
+  });
+
+  if (goal) {
+    return res.status(201).json({ message: "New goal created" });
+  } else {
+    return res.status(400).json({ message: "Invalid goal data received" });
+  }
+};
+
+module.exports = { createNewGoal };

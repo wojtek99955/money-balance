1bbffd1b06030a782f0b5c13963b3073b79946f2
wojtek99955 <wojtek99955@gmail.com>
2022-12-10 00:58:57 +0100

    style update

--- client/src/assets/atoms/Button.ts
+++ client/src/assets/atoms/Button.ts
@@ -4,7 +4,7 @@ export const Button = styled.button`
   background-color: ${({ theme }) => theme.colors.main.default};
   border: none;
   border-radius: 12px;
-  padding: 1rem 2.5rem;
+  padding: 0.8rem 1.5rem;
   font-size: 1rem;
   color: white;
   cursor: pointer;

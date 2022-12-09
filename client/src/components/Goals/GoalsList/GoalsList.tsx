fe0f923778224bb6ff287b0c9450af7efec7ac17
wojtek99955 <wojtek99955@gmail.com>
2022-12-10 00:01:58 +0100

    add goal list

---
+++ client/src/components/Goals/GoalsList/GoalsList.tsx
@@ -0,0 +1,108 @@
+import styled from "styled-components";
+import { Doughnut } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  Filler,
+  ArcElement,
+} from "chart.js";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  Filler,
+  ArcElement
+);
+
+const GoalContainer = styled.div`
+  background-color: white;
+  border-radius: 12px;
+  padding: 1rem;
+  position: relative;
+`;
+
+const ListContainer = styled.section`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 1rem;
+  padding: 2rem 0;
+`;
+
+const ChartContainer = styled.div`
+  width: 5rem;
+  position: absolute;
+  right: 1rem;
+  top: 0;
+  bottom: 0;
+  margin: auto 0;
+  display: flex;
+  align-items: center;
+`;
+
+interface Props {
+  goals: any;
+}
+
+const GoalsList = ({ goals }: Props) => {
+  const goalsAmount = goals.map((goal: any) => goal.amount);
+  console.log(goalsAmount);
+
+  const depositAmount = goals.map((goal: any) => goal.deposit);
+
+  const chartOptions = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: false,
+      },
+      elements: {
+        arc: {
+          borderWidth: 0.5,
+        },
+      },
+    },
+  };
+  return (
+    <ListContainer>
+      {goals.map((goal: any) => {
+        return (
+          <GoalContainer>
+            <h3>{goal.description}</h3>
+            {goal.amount}
+            {goal.deposit}
+            <ChartContainer>
+              <Doughnut
+                data={{
+                  labels: ["Left", "Deposit"],
+                  datasets: [
+                    {
+                      data: [goal.amount - goal.deposit, goal.deposit],
+                      backgroundColor: ["grey", "green"],
+                    },
+                  ],
+                }}
+                options={chartOptions}
+              />
+            </ChartContainer>
+            <p>
+              ${goal.deposit} of {goal.amount}
+            </p>
+          </GoalContainer>
+        );
+      })}
+    </ListContainer>
+  );
+};
+
+export default GoalsList;

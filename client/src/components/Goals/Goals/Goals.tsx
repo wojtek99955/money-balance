58f1d18823c4d9c1e98e639146b92071a6210a11
wojtek99955 <wojtek99955@gmail.com>
2022-12-10 12:33:56 +0100

    refactor styles

--- client/src/components/Goals/Goals/Goals.tsx
+++ client/src/components/Goals/Goals/Goals.tsx
@@ -1,59 +1,16 @@
-import styled from "styled-components";
 import { RouteContainer } from "../../../assets/atoms/RouteContainer";
-import { IoMdAddCircleOutline } from "react-icons/io";
 import { useState } from "react";
 import AddGoalModal from "../AddGoalModal";
 import { AnimatePresence } from "framer-motion";
 import { useGetGoalsQuery } from "../../../api/goalSlice";
 import GoalsList from "../GoalsList/GoalsList";
-import { Button } from "../../../assets/atoms/Button";
-
-const NoGoalsWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 3rem;
-  margin-top: 4rem;
-  h3 {
-    font-size: 2rem;
-    text-align: center;
-  }
-
-  div {
-    display: flex;
-    align-items: center;
-    gap: 2rem;
-    justify-content: center;
-
-    span {
-      font-size: 1.2rem;
-    }
-  }
-`;
-
-const AddGoalIcon = styled(IoMdAddCircleOutline)`
-  font-size: 2.5rem;
-  color: ${({ theme }) => theme.colors.main.default};
-  cursor: pointer;
-`;
-
-const GoalsContainer = styled.div`
-  max-width: 1200px;
-  margin: auto;
-  position: relative;
-`;
-
-const StyledButton = styled(Button)`
-  position: absolute;
-  right: 0;
-  display: flex;
-  align-items: center;
-  gap: 1rem;
-`;
-
-const AddIconBtn = styled(IoMdAddCircleOutline)`
-  color: white;
-  font-size: 2rem;
-`;
+import {
+  NoGoalsWrapper,
+  AddGoalIcon,
+  GoalsContainer,
+  StyledButton,
+  AddIconBtn,
+} from "./GoalsStyle";
 
 const Goals = () => {
   const [openAddGoalModal, setOpenAddGoalModal] = useState(false);
---
+++ client/src/components/Goals/Goals/GoalsStyle.ts
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+import { IoMdAddCircleOutline } from "react-icons/io";
+import { Button } from "../../../assets/atoms/Button";
+
+export const NoGoalsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 3rem;
+  margin-top: 4rem;
+  h3 {
+    font-size: 2rem;
+    text-align: center;
+  }
+
+  div {
+    display: flex;
+    align-items: center;
+    gap: 2rem;
+    justify-content: center;
+
+    span {
+      font-size: 1.2rem;
+    }
+  }
+`;
+
+export const AddGoalIcon = styled(IoMdAddCircleOutline)`
+  font-size: 2.5rem;
+  color: ${({ theme }) => theme.colors.main.default};
+  cursor: pointer;
+`;
+
+export const GoalsContainer = styled.div`
+  max-width: 1200px;
+  margin: auto;
+  position: relative;
+`;
+
+export const StyledButton = styled(Button)`
+  position: absolute;
+  right: 0;
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+`;
+
+export const AddIconBtn = styled(IoMdAddCircleOutline)`
+  color: white;
+  font-size: 2rem;
+`;

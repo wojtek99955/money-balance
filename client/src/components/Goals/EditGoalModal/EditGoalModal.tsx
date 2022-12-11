a4aed4c21aea25154a3e51c613e32466cb2213b1
wojtek99955 <wojtek99955@gmail.com>
2022-12-11 12:43:48 +0100

    refactor edit goal modal style

--- client/src/components/Goals/EditGoalModal/EditGoalModal.tsx
+++ client/src/components/Goals/EditGoalModal/EditGoalModal.tsx
@@ -1,74 +1,14 @@
-import styled from "styled-components";
 import ReactDOM from "react-dom";
-import { RiCloseCircleLine } from "react-icons/ri";
-import { motion } from "framer-motion";
 import { Field, Formik, Form } from "formik";
 import { Button } from "../../../assets/atoms/Button";
 import { useUpdateGoalMutation } from "../../../api/goalSlice";
-
-export const Container = styled(motion.div)`
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.3);
-  position: fixed;
-  top: 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 11;
-`;
-
-export const FormContainer = styled(motion.div)`
-  background-color: white;
-  width: 400px;
-  height: auto;
-  padding: 2rem 0;
-  border-radius: 12px;
-  position: relative;
-  gap: 1rem;
-
-  h3 {
-    text-align: center;
-    font-size: 1.6rem;
-    padding-bottom: 2rem;
-  }
-
-  select {
-    display: block;
-    border: ${({ theme }) => `2px solid ${theme.colors.grey}`};
-    border-radius: 12px;
-    padding: 1rem;
-    width: 80%;
-    font-size: 1rem;
-  }
-`;
-
-export const CloseIcon = styled(RiCloseCircleLine)`
-  font-size: 1.6rem;
-  color: ${({ theme }) => theme.colors.grey};
-  cursor: pointer;
-  position: absolute;
-  top: 0.5rem;
-  right: 0.5rem;
-`;
-
-export const StyledField = styled(Field)`
-  display: block;
-  border: ${({ theme }) => `2px solid ${theme.colors.grey}`};
-  border-radius: 12px;
-  padding: 1rem;
-  width: 80%;
-  font-size: 1rem;
-  margin: auto;
-`;
-
-export const FormWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-  justify-content: center;
-  align-items: center;
-`;
+import {
+  Container,
+  FormContainer,
+  CloseIcon,
+  StyledField,
+  FormWrapper,
+} from "./EditGoalModalStyle";
 
 interface InitialValues {
   description: string;

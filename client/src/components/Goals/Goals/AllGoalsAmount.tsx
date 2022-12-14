abfef684d7cd67f0fb1cfd6176d0c0f0e1abe7da
wojtek99955 <wojtek99955@gmail.com>
2022-12-14 14:04:02 +0100

    update styles

--- client/src/components/Goals/Goals/AllGoalsAmount.tsx
+++ client/src/components/Goals/Goals/AllGoalsAmount.tsx
@@ -1,12 +1,29 @@
 import { useGetTotalAmountQuery } from "../../../api/goalSlice";
+import styled from "styled-components";
+
+const Container = styled.div`
+  max-width: 20rem;
+  margin: auto;
+  padding-top: 4rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  h3 {
+    color: ${({ theme }) => theme.colors.grey};
+  }
+  div {
+    font-size: 2rem;
+  }
+`;
 const AllGoalsAmount = () => {
   const { data: allGoalsAmount, isLoading } = useGetTotalAmountQuery(undefined);
   console.log(allGoalsAmount);
   return (
-    <div>
+    <Container>
       <h3>You saved</h3>
       <div>$ {allGoalsAmount}</div>
-    </div>
+    </Container>
   );
 };
 

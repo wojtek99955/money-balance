cc831c20caf5df70854e19672f8a57df54968d05
wojtek99955 <wojtek99955@gmail.com>
2022-12-22 16:52:11 +0100

    add days left

--- client/src/components/Goals/GoalsList/GoalsList.tsx
+++ client/src/components/Goals/GoalsList/GoalsList.tsx
@@ -21,6 +21,7 @@ import {
   AcheivedGoal,
   AcheivedIcon,
   AddDepositIcon,
+  DaysLeft,
 } from "./GoalsListStyle";
 import EditGoal from "../EditGoal/EditGoal";
 import { useState } from "react";
@@ -65,23 +66,25 @@ const GoalsList = ({ goals }: Props) => {
   const handleOpenAddDepositModal = () => {
     setOpenAddDepositModal(true);
   };
+
   return (
     <ListContainer>
-      {goals.map((goal: Goal) => {
+      {goals.map((goal: any) => {
         return (
-          <GoalContainer key={goal._id}>
-            <Category>{goal.category}</Category>
-            <Description>{goal.description}</Description>
+          <GoalContainer key={goal.goal._id}>
+            <Category>{goal.goal.category}</Category>
+            <Description>{goal.goal.description}</Description>
             <Amount>
-              ${goal.deposit} of ${goal.amount}
+              ${goal.goal.deposit} of ${goal.goal.amount}
               <AddDepositIcon
                 onClick={() => {
                   handleOpenAddDepositModal();
-                  setCurrentId(goal._id);
+                  setCurrentId(goal.goal._id);
                 }}
               />
             </Amount>
-            <EditGoal id={goal._id} />
+            <DaysLeft>{goal.daysLeft} days left</DaysLeft>
+            <EditGoal id={goal.goal._id} />
             <ChartContainer>
               {goal.deposit >= goal.amount ? (
                 <AcheivedGoal>
@@ -93,7 +96,10 @@ const GoalsList = ({ goals }: Props) => {
                     labels: ["Left", "Deposit"],
                     datasets: [
                       {
-                        data: [goal.amount - goal.deposit, goal.deposit],
+                        data: [
+                          goal.goal.amount - goal.goal.deposit,
+                          goal.goal.deposit,
+                        ],
                         backgroundColor: ["#DEE1E9", "#009F5F"],
                       },
                     ],

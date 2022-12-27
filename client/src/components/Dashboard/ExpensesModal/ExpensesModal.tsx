27b97d36fd764e59e4ea78711614be39457eeb4e
wojtek99955 <wojtek99955@gmail.com>
2022-12-27 12:48:24 +0100

    add placeholder

--- client/src/components/Dashboard/ExpensesModal/ExpensesModal.tsx
+++ client/src/components/Dashboard/ExpensesModal/ExpensesModal.tsx
@@ -75,7 +75,7 @@ const ExpensesModal = ({ setOpenExpensesModal }: Props) => {
                 <option value="gift">gift</option>
                 <option value="transportation">transportation</option>
               </Field>
-              <StyledField type="text" name="amount" />
+              <StyledField type="text" name="amount" placeholder="amount" />
               <Button type="submit">Save</Button>
             </FormWrapper>
           </Form>

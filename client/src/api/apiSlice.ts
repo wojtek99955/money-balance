14e8da82309240ee807a561b139994585d8eddf3
wojtek99955 <wojtek99955@gmail.com>
2023-01-11 13:05:11 +0100

    update url

--- client/src/api/apiSlice.ts
+++ client/src/api/apiSlice.ts
@@ -4,7 +4,7 @@ export const apiSlice = createApi({
   reducerPath: "expenses",
 
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://money-balance-backend.onrender.com",
+    baseUrl: "https://money-balance.netlify.app/",
     credentials: "include",
   }),
   tagTypes: ["Expenses", "Incomes", "Avatar", "User", "Goals", "GoalPayment"],

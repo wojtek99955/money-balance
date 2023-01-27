ae639337bbf432a2c99acb325892bdf8b34180a9
wojtek99955 <wojtek99955@gmail.com>
2023-01-27 14:47:33 +0100

    update content

--- client/src/components/auth/DescriptionSection.tsx
+++ client/src/components/auth/DescriptionSection.tsx
@@ -1,20 +1,26 @@
 import Logo from "../../assets/atoms/Logo";
 import Charts from "../../assets/svg/Charts";
-import { Wrapper, Description, SvgContainer } from "./AuthStyles";
+import {
+  Wrapper,
+  Description,
+  SvgContainer,
+  DescriptionContainer,
+} from "./AuthStyles";
 
 const DescriptionSection = () => {
   return (
     <Wrapper>
-      <Description>
-        <Logo />
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Est quaerat
-          facilis, asperiores eos dicta error.
-        </p>
-      </Description>
-      <SvgContainer>
-        <Charts />
-      </SvgContainer>
+      <DescriptionContainer>
+        <Description>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est quaerat
+            facilis, asperiores eos dicta error.
+          </p>
+        </Description>
+        <SvgContainer>
+          <Charts />
+        </SvgContainer>
+      </DescriptionContainer>
     </Wrapper>
   );
 };
